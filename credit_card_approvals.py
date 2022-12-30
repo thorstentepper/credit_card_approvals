a67a61c1c6aefada111bf1075b73896e23fa5a86
Thorsten Tepper <84246508+thorstentepper@users.noreply.github.com>
2022-12-30 23:17:00 +0100

    Add Python script

---
+++ credit_card_approvals.py
@@ -0,0 +1,133 @@
+# Import Libraries
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+
+
+# Exploratory Data Analysis
+
+# Load the data
+cc_apps = pd.read_csv("datasets/cc_approvals.data", header=None)
+
+# Inspect the data
+cc_apps.head()
+
+# Print summary statistics
+cc_apps_description = cc_apps.describe()
+print(cc_apps_description)
+print("\n")
+
+# Print DataFrame information
+cc_apps_info = cc_apps.info()
+print(cc_apps_info)
+print("\n")
+
+# Inspect missing values
+print(cc_apps.tail(17))
+
+
+# Data Cleaning
+
+# Replace '?' with NaN
+cc_apps = cc_apps.replace("?", np.nan)
+
+# Inspect the missing values again
+print(cc_apps.tail(17))
+
+# Impute the missing values with mean imputation
+cc_apps.fillna(cc_apps.mean(), inplace=True)
+
+# Count the number of NaNs to verify the result
+print(cc_apps.isnull().sum())
+
+# Iterate over each column of cc_apps
+for col in cc_apps:
+    # Check if the column is of type 'object'
+    if cc_apps[col].dtypes == "object":
+        # Impute with the most frequent value
+        cc_apps = cc_apps.fillna(cc_apps[col].value_counts().index[0])
+
+# Count the number of NaNs and print the counts to verify
+print(cc_apps.isnull().sum())
+
+# Instantiate LabelEncoder
+le = LabelEncoder()
+
+# Iterate over all the values of each column and extract their dtypes
+for col in cc_apps.columns.to_numpy():
+    # Compare if the dtype is object
+    if cc_apps[col].dtypes == "object":
+        # Use LabelEncoder to do the numeric transformation
+        cc_apps[col] = le.fit_transform(cc_apps[col])
+
+# Drop the features 11 and 13 and convert the DataFrame to a NumPy array
+cc_apps = cc_apps.drop([11, 13], axis=1)
+cc_apps = cc_apps.to_numpy()
+
+# Segregate features and labels into separate variables
+X, y = cc_apps[:, 0:13], cc_apps[:, 13]
+
+
+# Building a Logistic-Regression Model
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42
+)
+
+# Instantiate MinMaxScaler and use it to rescale X_train and X_test
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledX_train = scaler.fit_transform(X_train)
+rescaledX_test = scaler.transform(X_test)
+
+# Instantiate a LogisticRegression classifier with default parameter values
+logreg = LogisticRegression()
+
+# Fit logreg to the train set
+logreg.fit(rescaledX_train, y_train)
+
+# Use logreg to predict instances from the test set and store it
+y_pred = logreg.predict(rescaledX_test)
+
+# Get the logreg model's accuracy score and print it
+print(
+    "Accuracy of logistic regression classifier: ",
+    logreg.score(rescaledX_test, y_test),
+)
+
+
+# Model Improvement
+
+# Print the confusion matrix of the logreg model
+print(confusion_matrix(y_test, y_pred))
+
+# Define the grid of values for tol and max_iter
+tol = [0.01, 0.001, 0.0001]
+max_iter = [100, 150, 200]
+
+# Create a dictionary in which tol and max_iter are keys and the lists of their
+# values are corresponding values
+param_grid = dict(tol=tol, max_iter=max_iter)
+
+# Instantiate GridSearchCV with the required parameters
+grid_model = GridSearchCV(estimator=logreg, param_grid=param_grid, cv=5)
+
+# Use scaler to rescale X and assign it to rescaledX
+rescaledX = scaler.fit_transform(X)
+
+# Fit the data to grid_model
+grid_model_result = grid_model.fit(rescaledX, y)
+
+# Summarize the results
+best_score, best_params = (
+    grid_model_result.best_score_,
+    grid_model_result.best_params_,
+)
+
+print("Best: %f using %s" % (best_score, best_params))
